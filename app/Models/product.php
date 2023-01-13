1d64c1c5281535a627d9ad529b78ee9b574ecede
iteleh <iteleh97@gmail.com>
2023-01-13 10:05:35 +0100

    Relationship created

--- app/Models/product.php
+++ app/Models/product.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\review;
 
 class product extends Model
 {
     use HasFactory;
+
+    public function reviews()
+    {
+        return $this->hasMany(review::class);
+    }
 }
