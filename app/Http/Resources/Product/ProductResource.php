ff073d970845ee6bd72467446cb0a528934b9deb
iteleh <iteleh97@gmail.com>
2023-01-13 11:12:19 +0100

    Modified Product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount'=>$this->discount
+            'totalPrice' => round((1-($this->discount/100)) * $this->price,2),
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'discount'=>$this->discount,
+            'rating' => $this->reviews->count() > 0 ?  round($this->reviews->sum('star')/$this->reviews->count()) : '0',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
